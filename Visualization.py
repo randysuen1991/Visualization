de6311cd5d345a7af2debc99a5ebbf2ebb9bdc62
randysuen1991 <randysuen1991@gmail.com>
2018-06-11 09:20:06 +0800

    new

--- Visualization.py
+++ Visualization.py
@@ -1,29 +1,61 @@
+import sys
+if 'C:\\Users\\ASUS\Dropbox\\pycode\\mine\\Dimension-Reduction-Approaches' not in sys.path :
+    sys.path.append('C:\\Users\\ASUS\Dropbox\\pycode\\mine\\Dimension-Reduction-Approaches')
 from sklearn.manifold import TSNE
-
-
+import matplotlib.pyplot as plt
+import DimensionReductionApproaches as DRA
+import numpy as np
 
 class Visualization():
-    def __init__(self):
+    def __init__(self,preprocess_components):
         self.visualizer = None
+        self.preprocess_components = preprocess_components
         
-    def Fit(self,X):
-        self.visualizer.fit(X)
+    # Notice: the Y should be numbers qual to or bigger than 1
+    def Fit(self,X,Y):
+        if self.preprocess_components != None :
+            self.pca_subspace = DRA.DimensionReduction.PCA(X)
+            self.preprocess_X = np.matmul(X,self.pca_subspace)
+        else :
+            self.preprocess_X = X
+        self.visualizer.fit(self.preprocess_X)
+        self.labels = Y 
         
-    def Fit_Transform(self,X):
-        self.visualizer.fit_transform(X)
+    def Fit_Transform(self,X,Y):
+        if self.preprocess_components != None :
+            self.pca_subspace = DRA.DimensionReduction.PCA(X)
+            self.preprocess_X = np.matmul(X,self.pca_subspace)
+        else :
+            self.preprocess_X = X
+        self.X_2d = self.visualizer.fit_transform(X)
+        self.labels = Y 
         
     def Visualize(self):
-        pass
+        target_ids = range(int(max(self.labels.ravel())))
+        colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
+        for i, c in zip(target_ids, colors):
+            plt.scatter(self.X_2d[self.labels == i+1, 0], self.X_2d[self.labels == i+1, 1], label=i)
+        plt.legend()
+        plt.show()
     
-    
-def tSNE():
-    def __init__(self):
+class tSNE(Visualization):
+    def __init__(self,preprocess_components=None):
+        super().__init__(preprocess_components)
         self.visualizer = TSNE()
-    def Fit(self,X):
-        self.visualizer.fit(X)
-    def Fit_Transform(self,X):
-        self.visualizer.fit_transform(X)
+
         
-    
+
+
+def test():
+    from sklearn import datasets
+    digits = datasets.load_digits()
+    # Take the first 500 data points: it's hard to see 1500 points
+    X = digits.data[:500]
+    y = digits.target[:500]
+    target_ids = range(len(digits.target_names))
+
+    print(digits.target_names,target_ids)
+
         
-        
+if __name__ == "__main__" :
+    test()
